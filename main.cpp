1467cf675674a7e321ae47c2580d23f5251b83e9
viydal <viydal04@gmail.com>
2024-10-02 16:09:12 +0930

    assignment3

--- main.cpp
+++ main.cpp
@@ -1,33 +1,108 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <tuple>
 #include <vector>
 
 class hashTable {
 protected:
-  std::vector<int> vectorStrings(26, 0);
+  std::vector<std::tuple<std::string, std::string>> table;
 
 public:
-  hashTable() { vectorStrings(26, std::make_tuple("NULL", "NULL")); }
-
-  bool searchKey(std::string key) {}
-
-  void insertValue(std::string key) {
-    int hashKey = hashFunction(key);
-    while (/* condition */) {
-      /* code */
+  hashTable() {
+    for (int i = 0; i < 26; i++) {
+      table.push_back(std::make_tuple("NULL", "never used"));
     }
   }
 
-  void deleteValue(std::string key) {}
+  int searchKey(std::string key) {
+    int hashKey = hashFunction(key);
+
+    int counter = 0;
+    while (counter < 26) {
+      if (std::get<1>(table[hashKey]) == "never used") {
+        return hashKey;
+      } else {
+        hashKey++;
+        if (hashKey > 25) {
+          hashKey = 0;
+        }
+        counter++;
+      }
+    }
+
+    return -1;
+  }
+
+  void insertValue(std::string key) {
+    std::tuple newTuple = std::make_tuple(key, "occupied");
+
+    int pos = searchKey(key);
+
+    if (pos == -1) {
+      std::cout << "NOT ENOUGH SPACE." << std::endl;
+      return;
+    } else {
+      table[pos] = newTuple;
+    }
+  }
+
+  void deleteValue(std::string key) {
+    int hashKey = hashFunction(key);
+
+    int counter = 0;
+    while (counter < 26) {
+      if (std::get<1>(table[hashKey]) == "never used") {
+        return;
+      } else {
+        if (std::get<0>(table[hashKey]) == key) {
+          std::get<1>(table[hashKey]) = "tombstone";
+          return;
+        }
+        hashKey++;
+        if (hashKey > 25) {
+          hashKey = 0;
+        }
+        counter++;
+      }
+    }
+  }
 
   int hashFunction(std::string key) {
     int hashKey = key[key.length() - 1] - 'a';
     return hashKey;
   }
+
+  void printTable() {
+    for (size_t i = 0; i < table.size(); i++) {
+      std::string key = std::get<0>(table[i]);
+      std::string status = std::get<1>(table[i]);
+
+      if (status == "occupied") {
+        std::cout << key << " ";
+      }
+    }
+  }
 };
 
 int main() {
   hashTable hash;
-  std::cout << hash.hashFunction("apple") << std::endl;
+
+  std::string input;
+  std::getline(std::cin, input);
+
+  std::stringstream ss(input);
+
+  std::string currentInstruction;
+  while (ss >> currentInstruction) {
+    if (currentInstruction[0] == 'A') {
+      hash.insertValue(
+          currentInstruction.substr(1, currentInstruction.length() - 1));
+    } else if (currentInstruction[0] == 'D') {
+      hash.deleteValue(
+          currentInstruction.substr(1, currentInstruction.length() - 1));
+    }
+  }
+
+  hash.printTable();
 }
---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <vector>
+
+int main() {
+    std::vector<std::tuple<std::string, std::string>> vectorStrings;
+}
