f6550e086cd5c4e6fc7b093ad79b86d855a8db0b
viydal <viydal04@gmail.com>
2024-10-23 17:01:08 +1030

    assignment4

--- main.cpp
+++ main.cpp
@@ -26,24 +26,6 @@ public:
     }
   }
 
-  bool linkInitialise(int node1, int node2) {
-    int root1 = find(node1);
-    int root2 = find(node2);
-
-    if (root1 != root2) {
-      if (seniority[root1] < seniority[root2]) {
-        parent[root1] = root2;
-      } else if (seniority[root1] > seniority[root2]) {
-        parent[root2] = root1;
-      } else {
-        parent[root2] = root1;
-        seniority[root1]++;
-      }
-      return true;
-    }
-    return false;
-  }
-
   bool link(int node1, int node2) {
     int root1 = find(node1);
     int root2 = find(node2);
@@ -61,6 +43,15 @@ public:
     }
     return false;
   }
+
+  void reset() {
+    int n = parent.size();
+    for (int i = 0; i < n; ++i) {
+      parent[i] = i;
+      seniority[i] = 0;
+    }
+  }
+
 
   void printPandS() {
     for (size_t i = 0; i < parent.size(); i++) {
@@ -142,37 +133,83 @@ public:
     UnionFind uf(cityConnections.size());
 
     std::vector<Edge> buildEdges = createEdges(buildCost);
-    std::vector<Edge> destroyEdges = createEdges(buildCost);  
+    std::vector<Edge> destroyEdges = createEdges(destroyCost);  
 
-    printEdges(buildEdges);
+    // printEdges(buildEdges);
+    // std::cout << std::endl;
     // printEdges(destroyEdges);
 
-    for (size_t i = 0; i < cityConnections.size(); ++i) {
-      for (size_t j = 0; j < cityConnections[i].size(); ++j) {
-        if (cityConnections[i][j] == '1') {
-          if (uf.linkInitialise(i, j)) {
-            std::cout << "SUCCESSFUL CONNECTION | connected node1: " << i << ", to node2: " << j << std::endl;
-          } else {
-            std::cout << "CONNECTION FAILURE | connected node1: " << i << ", to node2: " << j << std::endl;
-          }
-        }
-      }
-    }
+    int weight = initialWeight(cityConnections, buildEdges, destroyEdges, uf);
 
-    uf.printPandS();
-
-    int weight = 0;
+    // uf.printPandS();
 
     for (size_t i = 0; i < buildEdges.size(); i++) {
       if (uf.find(buildEdges[i].a) != uf.find(buildEdges[i].b)) {
+        std::cout << "ADDING CONNECTION | " << buildEdges[i].a << " to " << buildEdges[i].b << " with cost " << buildEdges[i].weight << std::endl;
         weight += buildEdges[i].weight;
         uf.link(buildEdges[i].a, buildEdges[i].b);
       }
     }
 
-    uf.printPandS();
+    // uf.printPandS();
 
     return weight;
+  }
+
+  int initialWeight(std::vector<std::string> cityConnections, std::vector<Edge> buildEdges, std::vector<Edge> destroyEdges, UnionFind &uf) {
+    std::vector<Edge> perfectEdges;
+
+    printEdges(destroyEdges);
+
+    int initialWeight = 0;
+
+    for (size_t i = 0; i < cityConnections.size(); i++) {
+      for (size_t j = i; j < cityConnections[i].size(); j++) {
+        if (cityConnections[i][j] == '1') {
+          int index1 = i;
+          int index2 = j;
+
+          if (uf.find(index1) != uf.find(index2)) {
+            for (size_t k = 0; k < destroyEdges.size(); k++) {
+              if ((destroyEdges[k].a == index1 && destroyEdges[k].b == index2) || (destroyEdges[k].b == index1 && destroyEdges[k].a == index2)) {
+                std::cout << "ADDING CONNECTION | " << destroyEdges[k].a << " to " << destroyEdges[k].b << std::endl;
+                perfectEdges.push_back(destroyEdges[k]);
+
+                uf.link(index1, index2);
+
+                uf.printPandS();
+                std::cout << std::endl;
+              }
+            }
+          } else {
+            int root = uf.find(index1);
+
+            std::cout << "ATTEMPTING SWAP | " << index1 << " to " << index2 << std::endl;
+            for (size_t k = 0; k < perfectEdges.size(); k++) {
+              if (uf.find(perfectEdges[k].a) == root || uf.find(perfectEdges[k].b) == root) {
+                std::cout << "SWAP INTIATED | " << index1 << " to " << index2 << " with cost " << perfectEdges[k].weight << std::endl;
+                initialWeight += perfectEdges[k].weight;
+
+                perfectEdges.erase(perfectEdges.begin() + k);
+
+                uf.reset();
+
+                for (size_t l = 0; l < perfectEdges.size(); l++) {
+                  if (uf.find(index1) != uf.find(index2)) {
+                    uf.link(perfectEdges[l].a, perfectEdges[l].b);
+                  }
+                }
+                uf.link(index1, index2);
+
+                uf.printPandS();
+              }
+            }
+          }
+        }
+      }
+    }
+    
+    return initialWeight;
   }
 
   void printEdges(std::vector<Edge> edges) {
@@ -187,9 +224,9 @@ public:
 int main() {
   MST mst;
 
-  std::vector<std::string> cityConnections = {"000", "000", "000"};
-  std::vector<std::string> buildCost = {"ABD", "BAC", "DCA"};
-  std::vector<std::string> destroyCost = {"ABD", "BAC", "DCA"};
+  std::vector<std::string> cityConnections = {"011000","101000","110000","000011","000101","000110"};
+  std::vector<std::string> buildCost = {"ABDFFF","BACFFF","DCAFFF","FFFABD","FFFBAC","FFFDCA"};
+  std::vector<std::string> destroyCost = {"ABDFFF","BACFFF","DCAFFF","FFFABD","FFFBAC","FFFDCA"};
 
   std::cout << mst.createMST(cityConnections, buildCost, destroyCost) << std::endl;
 }
